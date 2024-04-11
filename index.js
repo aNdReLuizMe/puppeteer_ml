115c891ace72e309129051c1d9a199a442295983
André Luiz <andreluiz.me@gmail.com>
2024-04-11 14:33:38 -0400

    feat: Adição do .gitignore

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const pup = require("puppeteer");
+const pup = require('puppeteer');
 
-const url = "https://www.mercadolivre.com.br";
-const searchFor = "macbook";
+const url = 'https://www.mercadolivre.com.br';
+const searchFor = 'macbook';
 
 (async () => {
   const browser = await pup.launch({ headless: false });
   //abre o browser - headless quer dizer no backgroud, estando falso vc ve tudo o que ocorre
   const page = await browser.newPage();
   //abre uma nova página
-  console.log("Iniciei!");
+  console.log('Iniciei!');
 
   await page.goto(url);
   //vai pra url desejada (const url)
-  console.log("Abrindo URL!");
+  console.log('Abrindo URL!');
 
-  await page.waitForSelector("#cb1-edit");
+  await page.waitForSelector('#cb1-edit');
   //espera pelo total carregamento do seletor desejado
 
-  await page.type("#cb1-edit", searchFor);
+  await page.type('#cb1-edit', searchFor);
   //vai até o campo do identificador (cb1-edit) e digita a variável desejada (searchFor)
 
   await Promise.all([
@@ -25,7 +25,7 @@ const searchFor = "macbook";
    page.click('.nav-search-btn')
   ]) //executa a navegação da página clicando no botão de pesquisa (nav-search-btn)
 
-  const links = await page.$$eval('.ui-search-item__brand-discoverability ui-search-item__group__element > a', el => el.map(link => link.href));
+  const links = await page.$$eval('.ui-search-item__image > a', el => el.map(link => link.href));
   console.log(links);
 
 //   await browser.close();
